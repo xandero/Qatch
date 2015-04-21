77a1c73a48ed54e3ae54a3e5da5686377aa1fc21
xandero <alexander.ohan@gmail.com>
2015-04-21 17:45:30 +1000

    backbone router

---
+++ app/assets/javascripts/router/router.js
@@ -0,0 +1,10 @@
+var app = app || {};
+
+// Kind of like the Rails router with embedded actions.
+// Makes the application navigatable, with meaningful URLs.
+app.AppRouter = Backbone.Router.extend({
+  routes: {
+    '': 'index',
+//  ???  'users/:id': 'makeReservation' ???
+  }
+});
